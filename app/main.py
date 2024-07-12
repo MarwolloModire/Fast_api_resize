dd47f9a8cd807234954896c1d8a1d2f9f62f5fb9
Marwollo.Modire <marwollo.modire@gmail.com>
2024-07-12 16:32:09 +0300

    Headers(user-agent and else)

--- app/main.py
+++ app/main.py
@@ -1,38 +1,21 @@
-from fastapi import FastAPI, Response, Cookie
-from fastapi.responses import JSONResponse
-from secrets import token_urlsafe
-from models.models import User
+from typing import Annotated
+
+from fastapi import FastAPI, Header, HTTPException
+import re
 
 app = FastAPI()
 
 
-arr = (('Patrick', 'Superstar'),
-       ('Bob', 'Crastysponge'),
-       ('user345', 'password345'))
-cookie_dict = {}
+@app.get('/headers')
+async def get_headers(user_agent: Annotated[str | None, Header()] = None,
+                      accept_language: Annotated[str | None, Header()] = None):
 
+    if not user_agent or not accept_language:
+        raise HTTPException(
+            status_code=400, detail='Required headers are missing')
 
-@app.post('/login')
-async def create_cookie(user: User):
-    if (user.username, user.password) in arr:
-        cookie_dict[ck_name := 'session_token'] = (
-            token := token_urlsafe(16)), user
-        response = JSONResponse(content={"message": "Login successful"})
-        response.set_cookie(key=ck_name, value=token,
-                            max_age=60, secure=True, httponly=True)
-        return response
-    return JSONResponse(content={"message": "User not found, please try again!"})
+    if not re.search(
+            r'.{2}-.{2},.{2};q=0\.\d,(.{2}|.{2}-.{2});q=0\.\d($|,.{2};q=0\.\d)', accept_language):
+        return HTTPException(400, 'Invalid Accept-Language format')
 
-
-@app.get('/user')
-async def read_items(session_token: str | None = Cookie(default=None)):
-    if session_token \
-            and (items := cookie_dict.get('session_token', None))\
-            and session_token == items[0]:
-        return items[1]
-    return JSONResponse(content={'message': 'Unauthorized'})
-
-
-@app.post('/logout')
-async def logout_user(response: Response):
-    response.delete_cookie('session_token')
+    return {'User-Agent': user_agent, 'Accept-Language': accept_language}
