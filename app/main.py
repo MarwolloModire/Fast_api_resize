699aca63c0d3a0ab9ec531139c00a97bfdda6436
Marwollo.Modire <marwollo.modire@gmail.com>
2024-07-16 13:47:29 +0300

    RBAC + JWT + encoding passwords + timeexpire tokens + Roles fro Users

--- app/main.py
+++ app/main.py
@@ -1,64 +1,122 @@
-from datetime import datetime, timedelta
-
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from models.models import User
+from datetime import datetime, timedelta, timezone
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from models.models import UserSchema, Role, Permission
 from secrets import token_urlsafe
 from passlib.context import CryptContext
 import jwt
+from typing import Annotated, Callable
+from functools import wraps
 
 
 app = FastAPI()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
 
 SECRET_KEY = token_urlsafe(16)
 ALGORITHM = 'HS256'
-# Устанавливаем 'время жизни' токена
-EXPIRATION_TIME = timedelta(minutes=1)
+EXPIRATION_TIME = timedelta(minutes=3)
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+USERS_DATA = {
+    'admin': {'username': 'admin', 'password': pwd_context.hash('adminpass'), 'role': 'admin'},
+    'user': {'username': 'user', 'password': pwd_context.hash('userpass'), 'role': 'user'},
+    'guest': {'username': 'guest', 'password': pwd_context.hash('guestpass'), 'role': 'guest'},
+}
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
-USERS_DATA = [
-    {'username': 'john_doe', 'password': 'securepassword123'}
-]
-# Хэшируем пароли в ДБ
-for h_p in USERS_DATA:
-    h_p['password'] = pwd_context.hash(h_p['password'])
+
+# Функция назначения разрешений при авторизации
+def set_permission(user: UserSchema):
+    if user.role == Role.ADMIN:
+        user.permission = Permission.ADMIN
+    elif user.role == Role.USER:
+        user.permission = Permission.USER
+    else:
+        user.permission = Permission.GUEST
+    return user
 
 
-# Функция создания токена с применением времени сгорания
+# Функция для создания JWT токена
 def create_jwt_token(data: dict):
-    data.update({'exp': datetime.utcnow() + EXPIRATION_TIME})
+    data.update({'exp': datetime.now(timezone.utc) + EXPIRATION_TIME})
     return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
 
 
-# Функция проверки пользователя (Аутентификация)
-def authenticate_user(username: str, password: str):
-    for user in USERS_DATA:
-        if user['username'] == username:
-            return pwd_context.verify(password, user['password'])
-    return False
-
-
-# Функция для проверки токена
-def verify_jwt_token(token: str = Depends(oauth2_scheme)):
+# Функция получения User'а по токену
+def get_user_from_token(token: str = Depends(oauth2_scheme)):
     try:
-        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[
+                             ALGORITHM])
+        return payload.get('sub')
     except jwt.ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail='The token has expired')
-    except jwt.PyJWTError:
-        raise HTTPException(status_code=401, detail='Invalid token')
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Token has expired',
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid token',
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
 
 
-# Роут для входа
-@app.post('/login')
-async def login(user_in: User):
-    if authenticate_user(user_in.username, user_in.password):
-        return {'access_token': create_jwt_token({'sub': user_in.username}), 'token_type': 'bearer'}
-    raise HTTPException(status_code=401, detail='Invalid credentials')
+# Функция для получения пользовательских данных на основе имени пользователя
+def get_user(username: str):
+    if username in USERS_DATA:
+        user_data = USERS_DATA[username]
+        return UserSchema(**user_data)
+    return None
 
 
-# Защищенный роут для получения информации о доступе
-@app.get('/protected_resource')
-async def about_me(verified_user: dict = Depends(verify_jwt_token)):
-    if verified_user:
-        return {'message': "Access granted! Let's GOOO!"}
+# Декоратор для проверки роли пользователя
+def role_required(role: Role):
+    def decorator(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            current_user: str = kwargs.get('current_user')
+            user_data = get_user(current_user)
+            if user_data.role != role:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN, detail='Not authorized'
+                )
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+# Роут для получения JWT-токена (так работает логин)
+@app.post('/token/')
+# тут логинимся через форму
+def login(user_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_data_from_db = get_user(user_data.username)
+    if user_data_from_db is None or pwd_context.verify(user_data.password, user_data_from_db.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid credentials',
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
+    set_permission(user_data_from_db)
+    access_token = create_jwt_token({'sub': user_data_from_db.username})
+    return {'access_token': access_token}
+
+
+# Защищенный роут для админов, когда токен уже получен
+@app.get('/admin/')
+@role_required(Role.ADMIN)
+def get_admin_info(current_user: str = Depends(get_user_from_token)):
+    return {'message': 'Welcome Admin!'}
+
+
+# Защищенный роут для обычных пользователей, когда токен уже получен
+@app.get('/user/')
+@role_required(Role.USER)
+def get_user_info(current_user: str = Depends(get_user_from_token)):
+    return {'message': 'Hello User!'}
+
+
+# Защищенный роут для гостей, когда токен уже получен
+@app.get('/guest/')
+@role_required(Role.GUEST)
+def get_guest_info(current_user: str = Depends(get_user_from_token)):
+    return {'message': 'Nice to meet you Guest!'}
--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel
+from enum import Enum
 
 
-class User(BaseModel):
+class Role(Enum):
+    ADMIN = 'admin'
+    USER = 'user'
+    GUEST = 'guest'
+
+
+class Permission(Enum):
+    ADMIN = 'create, read, update, delete'
+    USER = 'read, update'
+    GUEST = 'read'
+
+
+class UserSchema(BaseModel):
     username: str
     password: str
+    role: Role | None = None
+    permission: Permission | None = None
